8c3eedc788cbf87fd6b08938191544c71d65a0a1
张丘洋 <cs.qyzhang@gmail.com>
2019-02-24 15:27:04 +0800

    完成中断部分模块

--- MIPS-1.srcs/src/interrupt_handler.v
+++ MIPS-1.srcs/src/interrupt_handler.v
@@ -1,6 +1,11 @@
 `timescale 1ns / 1ps
 `include "MIPS-1.vh"
 
-module  InterruptHandler(clk,rst,interrupt_request);
-    output   epc;
+module  InterruptHandler(clk,rst,interrupt_request,epc,exception_handle_addr);
+    input       clk, rst, interrupt_request;
+    input[31:0] pc;
+    input[`ADDR_WIDTH-1:0]  exception_handle_addr;
+    output[31:0]epc;
+
+
 endmodule
